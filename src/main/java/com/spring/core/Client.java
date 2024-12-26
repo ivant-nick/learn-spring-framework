a0d00c102f80159b63b3150ccdd3097f9f45f6d7
CodeCrafter <ivan.turchyn94@gmail.com>
2024-12-26 17:43:10 +0100

    Annotation Based Configuration Example

---
+++ src/main/java/com/spring/core/Client.java
@@ -0,0 +1,31 @@
+package com.spring.core;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Client {
+
+    public static void main(String[] args) {
+
+//        Vehicle vehicle = new Cycle();
+//
+//        Traveler traveler = new Traveler(vehicle);
+//        traveler.startJourney();
+
+        //Creating Spring IOC Container
+        //Read the configuration class
+        //Create and manage the Spring beans
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        //Retrieve Spring Beans from IOC Container
+        Car car = applicationContext.getBean(Car.class);
+        car.move();
+
+        Bike bike = applicationContext.getBean(Bike.class);
+        bike.move();
+
+        Traveler traveler = applicationContext.getBean(Traveler.class);
+        traveler.startJourney();
+
+    }
+}
